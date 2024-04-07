7920fb5fe89c67f9d0616763773d8d8411cd63bf
yanjiangfeng <jiangfeng.yan@imaginelabs.space>
2024-04-08 00:49:27 +0800

    提交代码

--- src/modules/system/user/user.service.ts
+++ src/modules/system/user/user.service.ts
@@ -1,5 +1,19 @@
 import { AbstractService } from '@/common/abstract.service';
+import { SysUserEntity } from '@/entities/sys-user.entity';
+import { AppConfigService } from '@/shared/services/app-config.service';
 import { Injectable } from '@nestjs/common';
+import { Not } from 'typeorm';
 
 @Injectable()
-export class SystemUserService extends AbstractService {}
+export class SystemUserService extends AbstractService {
+  constructor(private readonly configService: AppConfigService) {
+    super();
+  }
+  async getUserByPage() {
+    const rows = await this.entityManager.find(SysUserEntity, {
+      where: {
+        id: Not(this.configService.appConfig.rootUserId),
+      },
+    });
+  }
+}
